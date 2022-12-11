bae0e22e690b86d2710899a8f10342639e199e2a
rkyzk <reikoyazaki@yahoo.com>
2022-12-11 13:40:37 +0000

    collect info regarding a new customer

---
+++ admin.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+print("****************************")
+print("          Hello!")
+print("****************************\n")
+fname = input("Enter the customer's first name: ")
+lname = input("Enter the customer's last name: ")
+print("\nAt which bank are you creating accounts?")
+print("Enter 'a' for North Bank\n'b' for East Bank\n'c' for South Bank.")
+code = input("Your input: ")
+svg_dp_str = input("\nInitial deposit in savings account: ")
+check_dp_str = input("Initial deposit in checking account: ")
+
+
+
